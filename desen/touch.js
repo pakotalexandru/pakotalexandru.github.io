0d2688f0d1daecd6f1cbaeaa971d28e6073152f2
pakotalexandru <43954743+pakotalexandru@users.noreply.github.com>
2018-11-26 11:23:57 +0200

    Add files via upload

--- desen/touch.js
+++ desen/touch.js
@@ -1,15 +1,17 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.2";
+document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.3";
 
 var canvas = doc.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
+
+var canvas_bounding_rect = canvas.getBoundingClientRect();
 
 function on_touch_start(e)
 {
 	for (var i = 0; i < e.changedTouches.Length; i++){
 		var contex = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX,
-				    e.changedTouches[i].pageY,
+		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
+				    e.changedTouches[i].pageY - canvas_bounding_rect.top,
 				   10,
 				   0,
 				   2 * Math.PI);
