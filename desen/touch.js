7437a9ecddd7c933650356d0345d05bd739f120e
pakotalexandru <43954743+pakotalexandru@users.noreply.github.com>
2018-11-26 11:09:42 +0200

    Add files via upload

--- desen/touch.js
+++ desen/touch.js
@@ -8,8 +8,8 @@ function on_touch_start (e)
 	for (var i = 0; i < e.changedTouches.Length; i++){
 		var contex = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches.item[i].pageX,
-				    e.changedTouches.item[i].pageY,
+		context.arc(e.changedTouches[i].pageX,
+				    e.changedTouches[i].pageY,
 				   10,
 				   0,
 				   2 * Math.PI);
