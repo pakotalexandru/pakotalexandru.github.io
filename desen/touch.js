305bd92590a6d76f9797cf8ef387f75acb357467
pakotalexandru <43954743+pakotalexandru@users.noreply.github.com>
2018-11-26 11:40:21 +0200

    Add files via upload

--- desen/touch.js
+++ desen/touch.js
@@ -1,37 +1,34 @@
-document.getElementById("id_business_version").innerHTML="Business version: 2018.11.26.4.11";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.5";
 
-var canvas = doc.getElementById("id_canvas");
+var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
+canvas.addEventListener("touchmove", on_touch_move);
 
-var canvas_bounding_rect = canvas.getBoundingClientRect();
+var canvas_boundig_rect = canvas.getBoundingClientRect();
 
-function on_touch_start(e)
-{
-	for (var i = 0; i < e.changedTouches.Length; i++){
+var last_pos = {x: 0, y: 0};
+
+function on_touch_start(e){
+	for(var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
-				    e.changedTouches[i].pageY - canvas_bounding_rect.top,
-				   10,
-				   0,
-				   2 * Math.PI);
-		context.stroke();		   
-		
+		context.moveTo(last_pos.x - canvas_boundig_rect.left, last_pos.y - canvas_boundig_rect.top);
+		context.lineTo(e.changedTouches[i].pageX - canvas_boundig_rect.left, e.changedTouches[i].pageY - canvas_boundig_rect.top);
+		context.arc(e.changedTouches[i].pageX - canvas_boundig_rect.left,
+					e.changedTouches[i].pageY - canvas_boundig_rect.top, 10, 0, 2 * Math.PI);
+		context.stroke();
 	}
 }
 
-//--------------------
-function on_touch_move(e)
-{
-	for (var i = 0; i < e.changedTouches.Length; i++){
+function on_touch_move(e){
+	e.preventDefault();
+	for(var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
-				    e.changedTouches[i].pageY - canvas_bounding_rect.top,
-				   10,
-				   0,
-				   2 * Math.PI);
-		context.stroke();		   
-		
+		context.arc(e.changedTouches[i].pageX - canvas_boundig_rect.left,
+					e.changedTouches[i].pageY - canvas_boundig_rect.top, 10, 0, 2 * Math.PI);
+		context.stroke();
+		last_pos.x = e.changedTouches[i].pageX
+		last_pos.y = e.changedTouches[i].pageY
 	}
 }
