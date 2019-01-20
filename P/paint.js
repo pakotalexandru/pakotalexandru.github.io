9530e35ec90c42252a8843eb348d61b74fa8efcf
pakotalexandru <43954743+pakotalexandru@users.noreply.github.com>
2019-01-20 19:57:16 +0200

    Add files via upload

--- P/paint.js
+++ P/paint.js
@@ -59,20 +59,6 @@ window.onload = function() {
     lasty = newy;
   }
 
-function iter(){if (l.length<10){l.push(one);}else{l.shift();l.push(one);}}
-function white(id){document.getElementById(id).style.background="white";}
-function color(colour){c.beginPath();c.strokeStyle=colour;c.fillStyle=colour;c.closePath();}
-function events(){b.onmousedown=md;b.onmouseup=mu;b.onmousemove=mv;}
-function md(e){one=c.getImageData(0,0,b.width,b.height);iter();img=c.getImageData(0,0,b.width,b.height);sX=e.x;sY=e.y;pulse="on";}
-function mu(e1){eX=e1.x;eY=e1.y;pulse="off";}
-function mv(e2){mX=e2.x;mY=e2.y;if (pulse=="on" && (item=='e' || item=='f')){draw();}else if (pulse=='on'){c.putImageData(img,0,0);draw();}}
-function rectangle(){events();item="a";}
-function circle(){events();item='b';}
-function line(){events();item='c';}
-function pencil(){events();item='d';}
-function erase(){item='e';events();}
-function spray(){item='f';events();}
-function undo(){if(l.length>=1){b.width=b.width;c.putImageData(l[l.length-1],0,0);l.pop();}}
 
   var clearButton = document.getElementById('clear')
   clearButton.onclick = clear
